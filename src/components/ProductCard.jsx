6fac270c22b64967234fb78aca6592f185d471b3
paul <amakapaul4cbn@gmail.com>
2025-07-19 10:42:51 -1200

    responsiveness added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 function ProductCard({product}) {
   return( <Link to={`/product/${product.id}`}>
-  <div className='shadow-lg rounded-md cursor-pointer'>
-    <img src={product.image} className='p-4 h-35' />
+  <div className='shadow-lg rounded-md cursor-pointer  max-w-sm mx-auto'>
+    <img src={product.image} className='w-full h-48 object-cover rounded-t-md' />
     <div className='bg-gray-50 p-4'>
-      <h2 className='text-lg font-semibold my-4'>{product.title.substring(0,15)+ "..."}</h2>
+      <h2 className='text-base md:text-lg font-semibold my-2'>{product.title.substring(0,15)+ "..."}</h2>
       <p className='text-sm text-zinc-500 border-b-2 pb-2'>{product.description.substring(0,25)+ "..."}</p>
       <div className='flex justify-between items-center mt-4'>
-        <p className='text-xl font-semibold'>${product.price}</p>
-        <p>View Details</p>
+        <p className='text-lg font-semibold'>${product.price}</p>
+        <p className='text-blue-600 hover:underline text-sm'>View Details</p>
       </div>
 
     </div>
